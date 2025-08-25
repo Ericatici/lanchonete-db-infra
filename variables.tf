039ced23d3c3f9b8e58fb4a485e6e262eb4bd4e9
Ericatici <79939834+Ericatici@users.noreply.github.com>
2025-08-25 17:20:07 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "db_username" {
+  type        = string
+  default     = "admin"
+  description = "Database username"
+}
+
+variable "db_password" {
+  type        = string
+  default     = "Password123!"
+  description = "Database password"
+}
