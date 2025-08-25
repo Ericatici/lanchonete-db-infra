f702c88d31d7d240173a5f6636e67a68c4d0fd8f
ericatici <erica.ticianelli@adejo.com.br>
2025-08-25 17:49:47 -0300

    Format Terraform files

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "lanchonete_db" {
-  allocated_storage    = 20
-  engine               = "postgres"
-  engine_version       = "15.3"
-  instance_class       = "db.t3.micro"
-  name                 = "lanchonetedb"
-  username             = "admin"
-  password             = "Password123!" # Em produção, use Secrets
-  skip_final_snapshot  = true
+  allocated_storage   = 20
+  engine              = "postgres"
+  engine_version      = "15.3"
+  instance_class      = "db.t3.micro"
+  name                = "lanchonetedb"
+  username            = "admin"
+  password            = "Password123!" # Em produção, use Secrets
+  skip_final_snapshot = true
 }
